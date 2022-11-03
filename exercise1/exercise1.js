7f37349e5581b321ea2515850969921a8eb2561b
Jake <45055732+JakeHelsel72@users.noreply.github.com>
2022-11-03 09:46:58 -0500

    exercise1 completed

--- exercise1/exercise1.js
+++ exercise1/exercise1.js
@@ -1,12 +1,20 @@
-//In JS
+/*Two password fields
+o first to enter the password and a second to verify it
+• A button labelled "Validate" that alerts one of the following messages:
+o Display informative error message if any of the following occur:
+ the passwords entered don't match
+ the passwords are not at least 8 characters long*/
 function validate()
 {
  var pass1 = document.getElementById("pass1").value;
  var pass2 = document.getElementById("pass2").value;
  console.log(pass1.length)
- if (pass1.length < 8)
- if (pass1 == pass2){
-    alert('Passwords match!')
+ if (pass1 != pass2){
+    alert('Error: Passwords entered don\'t match!')
+ } else if (pass1.length < 8){
+    alert('Error: Passwords are not at least 8 characters long!')
+ } else {
+    alert('Passwords match')
  }
 
 }
